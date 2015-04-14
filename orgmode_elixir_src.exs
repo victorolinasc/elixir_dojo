e1b18b801ab2696f0e274db22a129d4191b40fbd
Victor Olinasc! <victor.olinasc@gmail.com>
2015-04-14 10:02:53 -0300

    First part 2 writings on processes.

--- orgmode_elixir_src.exs
+++ orgmode_elixir_src.exs
@@ -1,12 +1,60 @@
-defmodule Usuario do
-  defstruct nome: "nome default"
+
+defmodule OlaMundo do
+
+  def start lista_inicial do
+    spawn __MODULE__, :loop, [lista_inicial]
+  end
+
+  def loop lista_de_pessoas do
+    receive do
+      {from, :lista_de_pessoas} ->
+        send from, {self, lista_de_pessoas}
+        # até aqui tudo bem... nada de novo
+        # mas como guardamos as pessoas para as quais demos olá?
+        # recursão :D
+        loop lista_de_pessoas    
+      :terminar ->
+        IO.puts "Terminando processo. Pessoas na lista: #{inspect lista_de_pessoas}"
+      {from, pessoa} ->
+        send from, {self, "Olá #{pessoa}"}
+        loop [ pessoa | lista_de_pessoas ]
+      _ ->
+        IO.puts "Ignorando mensagem"
+        loop lista_de_pessoas
+   
+    end
+  end
+
+  def ola(pid, pessoa) when is_binary(pessoa) do
+    send pid, {self, pessoa}    
+    receive do
+      {^pid, mensagem} -> IO.puts mensagem
+    after 
+      1500 -> IO.puts "Sem olá para você" # um truque novo! Podemos ter timeouts para não travar o shell.
+    end
+  end
+
+  def pessoas pid do
+    send pid, {self, :lista_de_pessoas}    
+    receive do
+      {^pid, lista} -> Enum.each lista, &(IO.puts "Pessoa: #{&1}") # sintaxe nova !!!
+    after 
+     1500 -> IO.puts "Sem pessoas para você"
+    end
+  end
+
+  def terminar pid do
+    send pid, :terminar # acabou o loop!
+  end
 end
 
-defmodule Test do
-  user = %Usuario{}
-  IO.puts "#{inspect user}"
+pid = OlaMundo.start []
 
-  # criando o memso usuário com um mapa dinamicamente
-  outro_user = %{__struct__: Usuario, nome: "Outro User"}
-  IO.puts "#{inspect outro_user}" # qual será o resultado?
-end
+OlaMundo.ola pid, "Fulano"
+OlaMundo.ola pid, "Beltrano"
+OlaMundo.ola pid, "Ciclano"
+
+OlaMundo.pessoas pid
+OlaMundo.terminar pid
+
+OlaMundo.ola pid, "Fulano" # timeout!
